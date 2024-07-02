5e082ecbd5fa2752bc1781190371c41e65c0bc78
Cleiton Lopes Braga <clopesbraga@gmail.com>
2024-07-02 10:42:07 -0300

    refactor(tela_lista): realizado ajustes no ApppModule e incluido endpoint para envio post

--- app/src/main/java/br/com/wsworks/listcarswswork/model/api/Car.kt
+++ app/src/main/java/br/com/wsworks/listcarswswork/model/api/Car.kt
@@ -1,15 +1,15 @@
-package br.com.wsworks.listcarswswork.model
+package br.com.wsworks.listcarswswork.model.api
 
 import com.google.gson.annotations.SerializedName
 
 data class Car(
+    @SerializedName("id") val id: Int,
+    @SerializedName("timestamp_cadastro") val timestamp_cadastro: Double,
+    @SerializedName("modelo_id") val modelo_id:Int,
     @SerializedName("ano") val ano: Int,
     @SerializedName("combustivel") val combustivel: String,
-    @SerializedName("cor") val cor: String,
-    @SerializedName("id") val id: Int,
-    @SerializedName("modelo_id") val modelo_id: Int,
-    @SerializedName("nome_modelo") val nome_modelo: String,
     @SerializedName("num_portas") val num_portas: Int,
-    @SerializedName("timestamp_cadastro") val timestamp_cadastro: Long,
+    @SerializedName("cor") val cor: String,
+    @SerializedName("nome_modelo") val nome_modelo: String,
     @SerializedName("valor")val valor: Double
 )
--- app/src/main/java/br/com/wsworks/listcarswswork/ui/screens/ListScreen.kt
+++ app/src/main/java/br/com/wsworks/listcarswswork/ui/screens/ListScreen.kt
@@ -1,6 +1,7 @@
 package br.com.wsworks.listcarswswork.ui.screens
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,9 +16,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import br.com.wsworks.listcarswswork.model.Car
-import br.com.wsworks.listcarswswork.model.ListCarsModel
+import br.com.wsworks.listcarswswork.model.api.Car
 import br.com.wsworks.listcarswswork.viewmodel.ListCarViewModel
+import com.google.gson.Gson
 import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -27,47 +28,45 @@ fun ListScreen(navController: NavHostController) {
     val viewModel = koinViewModel<ListCarViewModel>()
     val listcars: List<Car> by viewModel.listCarsState.collectAsState()
 
-
     viewModel.getListCars()
 
     val items: List<Car> = listcars.map { cars ->
-        Car(cars.ano,
-            cars.combustivel,
-            cars.cor,
-            cars.id,
-            cars.modelo_id,
-            cars.nome_modelo,
-            cars.num_portas,
+        Car(
+           cars.id,
             cars.timestamp_cadastro,
+            cars.modelo_id,
+            cars.ano,
+            cars.combustivel,
+            cars.num_portas,
+            cars.cor,
+            cars.nome_modelo,
             cars.valor
         )
     }
-
     LazyColumn {
         items(items) { car ->
-            CarItem(car)
-        }
-    }
-
-}
-
-@Composable
-fun CarItem(car: Car) {
-    Column(modifier = Modifier.padding(48.dp)) {
-        Card(modifier = Modifier.fillMaxWidth()) {
-            Column(
-                modifier = Modifier
+            Column(modifier = Modifier.padding(48.dp)) {
+                Card(modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text("Modelo: ${car.nome_modelo}")
-                Text("Ano:${car.ano}")
-                Text("Combustivel: ${car.combustivel}")
-                Text("Cor:${car.cor}")
-                Text("Id: ${car.id}")
-                Text("Modelo ID:${ car.modelo_id}")
+                    .clickable {
+                        val carJson = Gson().toJson(car)
+                        navController.navigate("detail/$carJson")
+                    }
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text("Modelo: ${car.nome_modelo}")
+                        Text("Ano:${car.ano}")
+                        Text("Combustivel: ${car.combustivel}")
+                        Text("Cor:${car.cor}")
+                    }
+                }
             }
         }
     }
+
 }
