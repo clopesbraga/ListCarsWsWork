71c9cb19830adbe3556909e667b9ddd4827716d3
Cleiton Lopes Braga <clopesbraga@gmail.com>
2024-07-02 10:37:29 -0300

    feature(tela_detalhe): incluido tela detalhe na navegação

--- app/src/main/java/br/com/wsworks/listcarswswork/ui/MainActivity.kt
+++ app/src/main/java/br/com/wsworks/listcarswswork/ui/MainActivity.kt
@@ -5,9 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import br.com.wsworks.listcarswswork.ui.screens.DetailScreen
 import br.com.wsworks.listcarswswork.ui.screens.ListScreen
 import br.com.wsworks.listcarswswork.ui.screens.LoginScreen
 
@@ -29,6 +32,13 @@ fun Navigation() {
     NavHost(navController = navController, startDestination = "login") {
         composable("login") { LoginScreen(navController) }
         composable("list") { ListScreen(navController) }
+        composable(
+            "detail/{car}",
+            arguments = listOf(navArgument("car"){ NavType.StringType})
+        ) { backStackEntry->
+            val car = backStackEntry.arguments?.getString("car")
+            DetailScreen(navController,car)
+        }
     }
 }
 
